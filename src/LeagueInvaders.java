028b7a07b1898cc4de39c6ff1dcf345e9587ecb2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-06-12 12:58:20 -0700

    ip LeagueInvaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import java.awt.Dimension;
+
+import javax.swing.*;
 
 public class LeagueInvaders {
-
+JFrame window;
+public static final int WIDTH=500;
+public static final int HEIGHT=700;
+public static void main(String[] args) {
+	LeagueInvaders l = new LeagueInvaders();
+	l.setup();
 }
+public LeagueInvaders(){
+	window=new JFrame();
+	
+}
+void setup() {
+	window.add(new GamePanel());
+	window.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+	window.setVisible(true);
+	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	window.pack();
+}
+}
+
